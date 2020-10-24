84da292d49ccab56b6f4b9a656e0e47ac3f3a38d
Alexis Navarro <37815457+ICKillerGH@users.noreply.github.com>
2020-10-24 12:25:13 -0400

    cambio de login de provincial

--- loginService.js
+++ loginService.js
@@ -11,22 +11,69 @@ function logIntoBancaribe(credentials) {
 }
 
 function logIntoProvincial(credentials) {
-    const usernameInput = document.getElementById('NUMDOCI');
-    const passwordInput = document.getElementById('claveIngreso');
-    const siguienteBtn = document.getElementById('BotonEnviar');
-    const submitBtn = document.getElementById('BotonIngresar');
-    
+    const usernameInput = document.getElementById('cedula');
+    const passwordInput = document.getElementById('password');
+    const submitBtn = document.getElementById('submit');
+    const spaceKeyEvent = new KeyboardEvent('keyup', {
+        bubbles: true,
+        cancelBubble: false,
+        cancelable: true,
+        charCode: 0,
+        code: "Space",
+        composed: true,
+        ctrlKey: false,
+        currentTarget: null,
+        defaultPrevented: false,
+        detail: 0,
+        eventPhase: 0,
+        isComposing: false,
+        isTrusted: true,
+        key: " ",
+        keyCode: 32,
+        location: 0,
+        metaKey: false,
+        repeat: false,
+        returnValue: true,
+        shiftKey: false,
+        type: "keyup",
+        which: 32
+    });
+    const backKeyEvent = new KeyboardEvent('keyup', {
+        bubbles: true,
+        cancelBubble: false,
+        cancelable: true,
+        charCode: 0,
+        code: "Backspace",
+        composed: true,
+        ctrlKey: false,
+        currentTarget: null,
+        defaultPrevented: false,
+        detail: 0,
+        eventPhase: 0,
+        isComposing: false,
+        isTrusted: true,
+        key: "Backspace",
+        keyCode: 8,
+        location: 0,
+        metaKey: false,
+        repeat: false,
+        returnValue: true,
+        shiftKey: false,
+        type: "keyup",
+        which: 8,
+    });
+
     usernameInput.focus();
-
     usernameInput.value = credentials.username;
-
-    siguienteBtn.click();
-
-    passwordInput.focus();
+    usernameInput.dispatchEvent(spaceKeyEvent);
+    usernameInput.dispatchEvent(backKeyEvent);
     
+    passwordInput.focus();
     passwordInput.value = credentials.password;
+    passwordInput.dispatchEvent(spaceKeyEvent);
+    passwordInput.dispatchEvent(backKeyEvent);
 
-    submitBtn.click();
+    submitBtn.focus();
 }
 
 function login(credentials) {
